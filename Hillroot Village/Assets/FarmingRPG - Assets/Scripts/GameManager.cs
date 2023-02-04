1bd66ae939cc4e52cfdf953ecdc47f21d808d68e
Laurender <laurileponiemi@gmail.com>
2023-02-04 22:30:57 +0200

    Tutorial complete

--- Hillroot Village/Assets/FarmingRPG - Assets/Scripts/GameManager.cs
+++ Hillroot Village/Assets/FarmingRPG - Assets/Scripts/GameManager.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
@@ -10,6 +11,7 @@ public class GameManager : MonoBehaviour
     public int cropInventory;
 
     public CropData selectedCropToPlant;
+    public TextMeshProUGUI statsText;
 
     public event UnityAction onNewDay;
 
@@ -41,22 +43,29 @@ public class GameManager : MonoBehaviour
 
     public void SetNextDay()
     {
-
+        curDay++;
+        onNewDay?.Invoke();
+        UpdateStatsText();
     }
 
     public void OnPlantCrop(CropData crop)
     {
         cropInventory--;
+
+        UpdateStatsText();
     }
 
     public void OnHarvestCrop(CropData crop)
     {
         money += crop.sellPrice;
+        UpdateStatsText();
     }
 
     public void PurhaceCrop(CropData crop)
     {
-
+        money -= crop.purhacePrice;
+        cropInventory++;
+        UpdateStatsText();
     }
 
     public bool CanPlantCrop()
@@ -66,11 +75,14 @@ public class GameManager : MonoBehaviour
 
     public void OnBuyCropButton(CropData crop)
     {
-
+        if(money >= crop.purhacePrice)
+        {
+            PurhaceCrop(crop);
+        }
     }
 
     void UpdateStatsText()
     {
-
+        statsText.text = $"Day: {curDay}\nMoney: ${money}\nCrop Inventory: {cropInventory}";
     }
 }
